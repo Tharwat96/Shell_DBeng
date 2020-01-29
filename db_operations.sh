34dfc9674995d55bb586c93279b9939b71e28b37
Jaxon <mohamedmostels@gmail.com>
2020-01-29 18:42:44 +0200

    TLI using Whiptail is now added
    using whiptail instead of the normal selection menu (selects where placed inside whiptail menu)

--- db_operations.sh
+++ db_operations.sh
@@ -1,32 +1,133 @@
 #!/bin/bash
-select DBoperation in "Create Database" "List Databases" "Delete Database" "Use Database for table operations"
-do
-case $DBoperation in
-"Create Database") 
-	#Check if the parent directory for Database Engine exists, if not create one.
-	if [ ! -d "/home/$USER/DBeng" ]
-	then
-		mkdir /home/$USER/DBeng && cd /home/$USER/DBeng 
-	else 
-		cd /home/$USER/DBeng
+export scriptDir=$(pwd)
+
+function mainMenu() {
+	#Display welcome on first time use of the engine.
+	if [[ ! -d ~/DBeng ]]
+	then whiptail --title "Welcome to the DBeng project." --msgbox "Start by creating a Database" 8 45
 	fi
-	echo "Enter the Database name:"
-	read userInput
-	setDBname="$userInput.beng" #Database name always ends with a .beng 
-	mkdir $setDBname 
-	echo "Database $setDBname created at `pwd` on `date`"	
-	;;
-	
+
+    dbOperations=$(whiptail --title "DBeng main Menu" --fb --menu "Choose an option" 15 60 6 \
+        "1" "Create Database" \
+        "2" "List Existing Databases" \
+		"3" "Delete Database" \
+        "4" "Do a table operation" \
+		"5" "Exit" 3>&1 1>&2 2>&3)
+
+    case $dbOperations in
+        1) #Create Database
+		#Check if the parent directory for Database Engine exists, if not create one.
+		if [[ ! -d ~/DBeng ]]
+		then mkdir ~/DBeng && cd ~/DBeng 
+		else cd ~/DBeng
+		fi
+
+		userInput=$(whiptail --inputbox "Enter the name of your Database:" 10 80 --title "Enter DB name"  3>&1 1>&2 2>&3)
+		setDBname="$userInput.beng" #Database name always ends with a .beng 
+		mkdir $setDBname 
+		whiptail --ok-button Done --msgbox "Database $setDBname created at `pwd` on `date`" 10 80 #10 = Heigjt 80 = Width
+        ;;
+
+        2) #List Databases		
+		if [[ ! -d ~/DBeng ]] #Check if no databases exist
+		then whiptail --title "No database found" --msgbox "Currently no Databases exist, start by creating one" 8 45
+		mainMenu #redisplay the menu
+
+		else
+			cd ~/DBeng
+			countDir=$(ls | wc -l) #Count how many databases currently exist
+			if [ $countDir -eq 0 ]
+			then whiptail --title "No databases exist in ~/DBeng" --msgbox "No databases to list" 8 45
+			else
+				whiptail --title "Found $countDir Databases" --msgbox "`find . -type d -name "*.beng" -printf "%f\n"`" 8 45
+				#List all the directories ending with .beng
+				#-printf changes find behavior, instead of outputing
+				#./directoryName this makes it output just directoryName
+			fi
+		fi
+		;;
+
+		3) #Delete Database
+		if [[ ! -d ~/DBeng  ]] 		#Check if parent database directory doesn't exist
+		then whiptail --title "No ~/DBeng directory found" --msgbox "Currently no Databases exist, start by creating one" 8 45
+		else 
+			cd ~/DBeng 
+			countDir=$(ls | wc -l) #Count how many databases currently exist
+			if [ $countDir -eq 0 ]
+			then whiptail --title "No databases exist in ~/DBeng" --msgbox "Create a Database first" 8 45
+			else
+				userInput=$(whiptail --inputbox "Enter the name of the Database to be deleted:" 10 80 --title "Delete Database"  3>&1 1>&2 2>&3)
+
+				#find if the database exist or not, grep is used to give the correct
+				#return code as find always returns 0 "Success" even if the directory doesn't exist
+				# 1> redirection hides the find command ouptput "./$userInput"
+				find . -type d -name "$userInput.beng" | grep $userInput 1> /dev/null
+				if [ ! $? -eq 0 ]
+				then whiptail --title "Database doesn't exist" --msgbox "No database named $userInput found." 8 45
+
+				#then echo "No database named \"$userInput\" was found!!"
+				else 
+					rm -rf "$userInput.beng" 
+					if [ $? -eq 0 ]
+					then  whiptail --title "Database Successfully removed" --msgbox "Database $userInput.beng was removed at `date`" 8 45
+					#echo "Database $userInput.beng was removed at `date`"
+					else
+					 	whiptail --title "Unknown error occured" --msgbox "For some reason we were unable to remove $userInput database" 8 45
+						#echo "An error occured during deletion"
+					fi
+				fi
+			fi
+		fi
+		;;
+
+		4) #Do a table operation
+		if [[ ! -d ~/DBeng  ]] 		#Check if parent database directory doesn't exist
+		then whiptail --title "No ~/DBeng directory found" --msgbox "Currently no Databases exist, start by creating one" 8 45
+		else 
+			cd ~/DBeng 
+			countDir=$(ls | wc -l) #Count how many databases currently exist
+			if [ $countDir -eq 0 ]
+			then whiptail --title "No databases exist in ~/DBeng" --msgbox "Create a Database first" 8 45
+			else userInput=$(whiptail --inputbox "Enter the name of the Database from the list\n `find . -type d -name "*.beng" -printf "%f\n"` " 10 80 --title "Table Operation"  3>&1 1>&2 2>&3)
+			#check if the name of the user input already exist
+			find . -type d -name "$userInput.beng" | grep $userInput 1> /dev/null
+			if [ ! $? -eq 0 ] #if it doesn't exist, prompt an error
+				then whiptail --title "Database name mismatch" --msgbox "no Database named $userInput found." 8 45
+				else whiptail --title "Cding" --msgbox "Good, now cding" 8 45
+				fi
+
+				#userInput=$(whiptail --inputbox "Enter the name of the Database to be deleted:" 10 80 --title "Delete Database"  3>&1 1>&2 2>&3)
+			fi 
+		fi
+		;;
+
+		5) exit
+		esac
+
+		mainMenu
+}
+
+mainMenu
+
+
+select DBoperation in "Create Database" "List Databases" "Delete Database" "Use Database for table operations" "Exit"
+do
+
+case $DBoperation in
+"Create Database") createDB
+;;	
+
 "List Databases") 
 	#Check if no databases exist
-	if [ ! -d "/home/$USER/DBeng" ]
-	then echo "Start by creating a Database first"
-	else 
-		cd /home/$USER/DBeng 
+	if [[ ! -d ~/DBeng ]]
+	then 
+		echo "Start by creating a Database first"
+	else
+		cd ~/DBeng
 		countDir=$(ls | wc -l) #Count how many databases currently exist
 		if [ $countDir -eq 0 ]
 		then echo "Currently no databases exist, create a Database first"
-		else 
+		else
 			echo "Available Databases: $countDir"
 			#List all the directories ending with .beng
 			#-printf changes find behavior, instead of outputing
@@ -34,13 +135,13 @@ case $DBoperation in
 			find . -type d -name "*.beng" -printf "%f\n"
 		fi
 	fi
-	;;
+;;
 "Delete Database") 
 	#Check if no databases exist
-	if [ ! -d "/home/$USER/DBeng" ]
+	if [[ ! -d ~/DBeng  ]]
 	then echo "Start by creating a Database first"
 	else 
-		cd /home/$USER/DBeng 
+		cd ~/DBeng 
 		countDir=$(ls | wc -l) #Count how many databases currently exist
 		if [ $countDir -eq 0 ]
 		then echo "Currently no databases exist, create a Database first"
@@ -63,13 +164,13 @@ case $DBoperation in
 			fi
 		fi
 	fi
-	;;
+;;
 "Use Database for table operations")
 	#Check if no databases exist
-	if [ ! -d "/home/$USER/DBeng" ]
+	if [[ ! -d ~/DBeng ]]
 	then echo "Start by creating a Database first"
 	else 
-		cd /home/$USER/DBeng 
+		cd ~/DBeng 
 		countDir=$(ls | wc -l) #Count how many databases currently exist
 		if [ $countDir -eq 0 ]
 		then echo "Currently no databases exist, create a Database first"
@@ -77,20 +178,17 @@ case $DBoperation in
 			echo "Select the database you want to do the operation on from the following:"
 			find . -type d -name "*.beng" -printf "%f\n"
 			read userInput 
-			export userInput
 			find . -type d -name "$userInput.beng" | grep $userInput 1> /dev/null
 			if [ ! $? -eq 0 ]
 			then echo "Please enter a correct DB name from the list"
 			else
-				# cd "$userInput.beng"
-				`bash tableRelatedOp.sh`
+				cd "$userInput.beng" && bash "$scriptDir/tableRelatedOp.sh"
 			fi
-			
-		
-		
 		fi
-	
 	fi
+;;
+"Exit")
+	exit
 ;;
 *) echo "Enter a valid choice from the list"
 esac 
--- tableRelatedOp.sh
+++ tableRelatedOp.sh
@@ -1,27 +1,67 @@
 #!/bin/bash
-userInput=test.beng
-cd ~/DBeng/$userInput
-select tableOperation in "Create table" "List existing tables" "Delete table"
+# exported variables: $scriptDir
+DBname=$(basename $(pwd)) #basename returns the last / directory from the pwd command (in our case the DB name)
+clear
+id=0;
+select tableOperation in "Create table" "List tables" "Delete table" "Modify table" "Go back to database menu"
 do
 case $tableOperation in
 "Create table")
+    clear
     echo "Enter table name:"
     read tableName
-    if [ -z $tableName ]
-    then 
-        echo "Please enter a table name"
+    if [ -z "$tableName" ]  # Check if string is empty using -z
+    then
+        echo "Table name can't be empty, please enter a table name"
     else
-        touch $tableName.tbeng
+        echo "$(pwd)/$tableName"
+        if [[ -f "$(pwd)/$tableName.tbeng" ]]; 
+        # checks if table is already created
+        then
+            clear
+            echo "This table is already created."
+        else
+            clear
+            touch "$tableName.tbeng"
+            echo "Table $tableName created at $(pwd) on $(date)"
+
+            echo -e "Enter column names [Separate each column name with a space]\n"
+            #Primary key handling
+            #resource used ==> https://stackoverflow.com/questions/9904980/variable-in-bash-script-that-keeps-it-value-from-the-last-time-running
+            if [ ! -f "~/id.var" ]; then #if the id file isn't present
+                id=1;
+            else 
+                id=`cat ~/id.var` #assign the id variable the value found in the id.var file
+            fi
+
+            read columnInput;
+            columnInputArray=($columnInput) #convert the input into array to iterate over the spaces
+            for column in "${columnInputArray[@]}"
+            do echo -e "$column:\c" >> $tableName.tbeng # \c for continuous text concatination (changing the default echo \n behavior)
+            done
+            echo -e "$id" >> $tableName.tbeng  #append the id to the .tbeng file
+            id=`expr ${id} + 1` #increment the id
+            echo "${id}" > ~/id.var #replace the id value with the incremented value.
+        fi
     fi
 ;;
-"List existing tables")
-    find . -type f -name "*.tbeng" -printf "%f\n" | cut -f1 -d.
+"List tables")
+        #Check if the directory contains no tables
+        #Count how many .tbeng files currently exist
+        countTables=$(ls | egrep '\.tbeng$' | wc -l) 
+        
+		if [ $countTables -eq 0 ]
+        then 
+            echo "$DBname doesn't contain any tables, create a table first"
+        else
+            find . -type f -name "*.tbeng" -printf "%f\n" | cut -f1 -d.
+        fi
 ;;
 "Delete table")
-    echo "*CAUTION ADVISED: This operation can't be undone*\nChoose a table to remove: \n"
+    echo -e "*CAUTION ADVISED: This operation can't be undone\nChoose a table to remove:\n"
     unset options i
-    while IFS= read -r -d $'\0' f; do
-        options[i++]="$f"
+    while IFS= read -r -d $'\0' fileName; do
+        options[i++]="$fileName"
     done < <(find . -maxdepth 1 -type f -name "*.tbeng" -print0 )
     select tableNames in "${options[@]}" "Back"
     do
@@ -38,6 +78,30 @@ case $tableOperation in
     esac
     done
 ;;
+
+"Modify table")
+    echo "select the table by typing its name: "
+    #List ops
+    echo "Select a table you want to do an operation on from the following:"
+    find . -type f -name "*.tbeng" -printf "%f\n"
+    unset userInput && read userInput 
+    find . -type f -name "$userInput.tbeng" | grep $userInput 1> /dev/null
+    if [ ! $? -eq 0 ]
+    then echo "Please enter a correct DB name from the list"
+    else
+        export selectedTable="$userInput.tbeng"
+        bash "$scriptDir/tableModification.sh"
+    fi
+
+    echo -e "1-Insert\n";
+    echo "Enter data type followed by column name: ";
+
+;;
+
+"Go back to database menu")
+    exit
+;;
+
 *)
 echo "Enter a valid input"
 esac
